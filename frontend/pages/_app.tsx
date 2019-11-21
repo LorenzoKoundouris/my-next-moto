428a2f616a3d01a92c5c421809e9f4f532aa980b
Lorenzo Koundouris <lorenzo.koundouris@gmail.com>
2019-11-21 21:00:49 +0000

    encapsulate all pages with custom wrapper

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import App, { AppContext } from 'next/app';
+import Page from '../components/Page';
+
+class MyApp extends App {
+  static async getInitialProps(appContext: AppContext) {
+    const appProps = await App.getInitialProps(appContext);
+    return { ...appProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    );
+  }
+}
+
+export default MyApp;
