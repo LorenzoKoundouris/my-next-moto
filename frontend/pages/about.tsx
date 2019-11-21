8b7e25d1d41eb83015ed3faaf6d17c8b2152ca06
Lorenzo Koundouris <lorenzo.koundouris@gmail.com>
2019-11-21 20:58:45 +0000

    adds index and about pages

---
+++ frontend/pages/about.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const About = () => (
+  <>
+    <StyledTitle>About My Next Moto</StyledTitle>
+  </>
+);
+
+const StyledTitle = styled.h1`
+  color: orange;
+`;
+
+export default About;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
+const Index = () => (
+  <>
+    <StyledTitle>My Next Moto</StyledTitle>
+  </>
+);
+
+const StyledTitle = styled.h1`
   color: red;
-  font-size: 50px;
 `;
 
-export default () => <Title>My next moto</Title>;
+export default Index;
