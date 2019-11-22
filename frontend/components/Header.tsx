ad48d160e56fd3657240eb74bbb4d62ddf45923d
Lorenzo Koundouris <lorenzo.koundouris@gmail.com>
2019-11-22 18:11:37 +0000

    refactor header to use components

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,31 +1,35 @@
 import styled from 'styled-components';
 
+import LinkItem from '../components/common/LinkItem';
+
 const Header = () => (
   <StyledHeader>
-    <MenuButton>🍔 MENU</MenuButton>
-    <MotorradLogo src='https://www.bmw-motorrad.co.uk/content/dam/bmwmotorradnsc/common/mnm/graphics/bmw_motorrad_logo.png.asset.1572270945089.png' />
+    <LinkItem href='/'>🍔MENU</LinkItem>
+    <LogoContainer>
+      <MotorradLogo
+        alt='BMW Motorrad Logo'
+        src='https://www.bmw-motorrad.co.uk/content/dam/bmwmotorradnsc/common/mnm/graphics/bmw_motorrad_logo.png.asset.1572270945089.png'
+      />
+    </LogoContainer>
   </StyledHeader>
 );
 
 const StyledHeader = styled.div`
   height: 85px;
   display: flex;
-  background: yellow;
+  background: yellow; /* set props.theme.primaryColour */
   justify-content: space-between;
   padding: 0 20px;
 `;
 
-const MenuButton = styled.button`
-  border: none;
-  background: transparent;
-  font-size: 20px;
-  padding: 0;
-  font-weight: bold;
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items: center;
 `;
 
 const MotorradLogo = styled.img`
   width: 140px;
-  margin: auto 0;
 `;
 
 export default Header;
