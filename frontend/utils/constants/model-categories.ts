802586fa2ac83c30301c35e90e554a297931da52
Lorenzo Koundouris <lorenzo.koundouris@gmail.com>
2019-11-24 16:23:29 +0000

    add model categories constants

---
+++ frontend/utils/constants/model-categories.ts
@@ -0,0 +1,8 @@
+// Model categories
+export const CATEGORY_ALL = 'All';
+export const CATEGORY_SPORT = 'Sport';
+export const CATEGORY_TOUR = 'Tour';
+export const CATEGORY_ROADSTER = 'Roadster';
+export const CATEGORY_HERITAGE = 'Heritage';
+export const CATEGORY_ADVENTURE = 'Adventure';
+export const CATEGORY_URBAN_MOBILITY = 'Urban Mobility';
